8c8250b1d5fbd8e7b24192879c89c04696db97f2
yawar101 <na55@students.kiron.ngo>
2017-05-09 23:07:19 +0200

    Update palindrome.cpp

--- solutions/palindrome.cpp
+++ solutions/palindrome.cpp
@@ -2,21 +2,23 @@
 
 using namespace std;
 int main ()
-{ int num, copy, digit, rev=0;
+{ 
+  int num, copy, digit, rev = 0;
 cout<<"Enter the number:";
 cin>>num;
 copy=num; // we reverse this variable named 'copy'
 
 while (num!=0)
-{ digit = num %10; // returns the last digit of number
-rev = (rev*10) + digit; 
-num = num/10;} // removes the last digit from number
+{ 
+  digit = num %10; // returns the last digit of number
+  rev = (rev*10) + digit; 
+  num = num/10;} // removes the last digit from number
 cout<<"The reverse of the number is:"<<rev;
 
 if(rev==copy) // compares both numbers
-cout<<"\nThe given number is palindrome";
+cout<<"\nThe given number is palindrome" <<endl;
 
 else
-cout<<"\n"<<"The given number is not a palindrome";
+cout<<"\n"<<"The given number is not a palindrome" <<endl;
 return 0;
 }
